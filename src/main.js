edad2c9d9669dcd7e12bfe96e012e04940dd5e27
pilotvitalik <itvkip@yandex.ru>
2019-12-12 15:29:50 +0300

    Added: connecting to Firebase

--- src/main.js
+++ src/main.js
@@ -3,12 +3,28 @@ import App from './App.vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { store } from './store/store';
+import firebase from 'firebase/app';
+import storage from 'firebase/storage';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueResource);
-
 Vue.use(VueRouter);
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDj4SJediSzhHRhxp3vqce0jIcEqoBFpP4",
+  authDomain: "fitnesshouse-ba47f.firebaseapp.com",
+  databaseURL: "https://fitnesshouse-ba47f.firebaseio.com",
+  projectId: "fitnesshouse-ba47f",
+  storageBucket: "fitnesshouse-ba47f.appspot.com",
+  messagingSenderId: "832820160033",
+  appId: "1:832820160033:web:67305f6327bea8a783e560",
+  measurementId: "G-MMLQPPQRWM"
+});
+
+const storageRef = firebaseApp.storage().ref();
+
+export default storageRef;
 
 new Vue({
   store,
--- src/store/store.js
+++ src/store/store.js
@@ -5,5 +5,5 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-  }
+  },
 })
